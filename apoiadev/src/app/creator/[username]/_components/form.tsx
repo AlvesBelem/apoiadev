4efbbb98a01fb7a8a70272fa6b0b3789a3e2081b
AlvesBelem <marcelocartoes.pa@gmail.com>
2025-08-31 12:39:03 -0300

    pagina do criador

---
+++ apoiadev/src/app/creator/[username]/_components/form.tsx
@@ -0,0 +1,103 @@
+"use client"
+import { z } from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Textarea } from "@/components/ui/textarea"
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { Label } from '@/components/ui/label';
+
+const formSchema = z.object({
+    name: z.string().min(1, "O nome é obrigatório"),
+    message: z.string().min(1, "A menssagem é obrigatória"),
+    price: z.enum(["2", "5", "10", "20", "30"], {
+        required_error: "O valor é obrigatório",
+    })
+})
+
+type FormData = z.infer<typeof formSchema>
+
+export function FormDonate() {
+    // 1. Define your form.
+    const form = useForm<FormData>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            message: "",
+            price: "2"
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 mt-5">
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Nome</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Digite seu nome..."
+                                    {...field}
+                                    className='bg-white'
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="message"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Mensagem</FormLabel>
+                            <FormControl>
+                                <Textarea
+                                    placeholder="Digite sua mensagem..."
+                                    {...field}
+                                    className='bg-white h-40 resize-none'
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="price"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Valor da doação</FormLabel>
+                            <FormControl>
+                                <RadioGroup
+                                    onValueChange={field.onChange}
+                                    defaultValue={field.value}
+                                    className='flex items-center gap-3'
+                                >
+                                    {["2", "5", "10", "20", "30"].map((value) => (
+                                        <div key={value} className="flex items-center gap-1">
+                                            <RadioGroupItem value={value} id={value} />
+                                            <Label className="text-lg" htmlFor={value}>R$ {value}</Label>
+                                        </div>
+                                    ))}
+                                </RadioGroup>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Fazer Doação</Button>
+            </form>
+        </Form>
+    )
+}
---
+++ apoiadev/src/app/creator/[username]/_data-access/get-info-user.tsx
@@ -0,0 +1,31 @@
+import { prisma } from "@/lib/prisma";
+import { z } from 'zod';
+
+const createUsernameSchema = z.object({
+    username: z.string({ message: "O username é obrigatório" })
+})
+
+type createUsernameSchema = z.infer<typeof createUsernameSchema>
+
+export async function getInfoUser(data: createUsernameSchema) {
+    const schema = createUsernameSchema.safeParse(data)
+
+    if (!schema.success) {
+        return null
+    }
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                username: data.username
+            }
+
+        })
+
+        return user;
+    } catch (err) {
+        return null
+    }
+
+}
+
